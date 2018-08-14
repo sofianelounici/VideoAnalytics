ad0dd2d68ef8295984cc625f8707632b911dfebb
Sofiascope <sofiane.lounici03@gmail.com>
2018-08-14 14:40:18 +0200

    Updated example

--- example.py
+++ example.py
@@ -1,8 +1,10 @@
 """ Boundary detection on the GoldenEye video"""
 
-goldeneye = VideoBoundaries()
-goldeneye.frame(2000,'goldeneye.mp4')
-goldeneye.detectCut(15)
+videoToDetect = VideoBoundaries()
+videoToDetect.frame(2000,'goldeneye.mp4')
+videoToDetect.detectCut(thres=0.6, minFrame=15)
 
 #Your output path
 goldeneye.extractClip(r'C:\Users\Sofiane\Desktop\Projects-Notebooks\SceneDetection\video', verbose=True)
+# Compute accuracy with a tolerance level concerning the frames
+recall, precision, f1 = goldeneye.accuracy(r'C:\Users\Sofiane\Desktop\Projects-Notebooks\VideoAnalytics\test_results_1600fr.txt', tolerance=2, verbose=True)
